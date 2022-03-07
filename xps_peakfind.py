b2cda8b0b351bf109598740fc6194a6fcc6d68df
jithesh82 <jithesh82@gmail.com>
2022-03-07 14:45:41 -0500

    adding updating xps_peakfind.py

--- xps_peakfind.py
+++ xps_peakfind.py
@@ -4,10 +4,22 @@ import pandas as pd
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
+plt.ion()
+
 df = pd.read_csv('Au.csv')
 df.columns = ['BE', 'Int']
-plt.plot(df['Int'], '.')
+prom = 1000
+while True:
+    plt.plot(df['Int'], '.')
 
-peaks, _ = find_peaks(df['Int'], prominence=2200)
-plt.plot(peaks, df['Int'][peaks], 'X')
-plt.show()
+    peaks, _ = find_peaks(df['Int'], prominence=prom)#, \
+        #threshold=1.5,\
+        #width=0.5, distance=10)
+    plt.plot(peaks, df['Int'][peaks], 'X')
+    prom = input('prom: ')
+    plt.clf()
+    if prom == 'q':
+        break
+    prom = int(prom)
+
+#plt.show()
