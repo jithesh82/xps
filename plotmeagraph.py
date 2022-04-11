d719660257acb8237f8851d65c2f62c0898e6a55
jithesh82 <jithesh82@gmail.com>
2022-04-11 10:29:39 -0400

    adding plotmeagraph.py

---
+++ plotmeagraph.py
@@ -0,0 +1,22 @@
+# a wrapper around matplotlib plot
+
+from matplotlib import pyplot as plt
+
+class Plot:
+    def __init__(self, x, y, xlabel='x',\ 
+            ylabel='y', legend=[], title='Graph x-y'):
+        self.x = x
+        self.y = y
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.legend = legend
+        self.title = title
+
+    def plot(self):
+        plt.plot(x, y)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.legend(self.legend)
+        plt.title(self.title)
+        plt.show()
+
