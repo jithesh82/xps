40862ea822989f38b56b78ef699fcb582b061d9b
jithesh82 <jithesh82@gmail.com>
2022-04-20 14:59:21 -0400

    updated cropmygraph.py to catch click on graph to choose bg end points

--- cropmygraph.py
+++ cropmygraph.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+plt.ion()
 
 def cropIt(df, xmin, xmax):
     # exclude values less than xmin
@@ -12,6 +13,20 @@ def cropIt(df, xmin, xmax):
     # exclude values greater than xmax
     df = df[df['x'] < xmax]
     return df
+
+# saves the clicked x, y data
+clicked = []
+def on_click(event):
+    """
+    To save the x, y data from right mouse click
+    """
+    if event.button is MouseButton.LEFT:
+        clicked.append((event.xdata, event.ydata))
+        print((event.xdata, event.ydata))
+
+# set up matplotlib for catching mouse clicks
+from matplotlib.backend_bases import MouseButton
+plt.connect('button_press_event', on_click)
 
 def cropMyGraph(data):
     # read the csv file
@@ -23,17 +38,22 @@ def cropMyGraph(data):
     df.columns = ['x', 'y']
     while True:
         # turn stdin on
-        plt.ion()
+        #plt.ion()
         plt.plot(df['x'], df['y'])
-        # visualy inspect graph and enter suitable
-        # crop points
-        xmin = input('xmin: ')
-        xmax = input('xmax: ')
+        # wait to choose the end points
+        wait = input('q to quit: ')
         # breakout of the infinite loop
-        if xmin == 'q': break
+        if wait == 'q': break
+        # visualy inspect graph and click suitable
+        # crop points
+        #xmin = input('xmin: ')
+        #xmax = input('xmax: ')
+        xmin = clicked[0][0]
+        xmax = clicked[1][0]
         # call cropIt with suitable crop points
         df = cropIt(df, float(xmin), float(xmax))
-    return df
+
+    return (df['x'], df['y'])
 
 # run as a top-level file by reading
 # data as commnadline argument
@@ -42,7 +62,8 @@ if __name__ == '__main__':
     if sys.argv[1] == '-h':
         print('Usage:  plot_csv.py mydata.csv ')
     else:
-    # visualize the data and choose crop points
-    # the returned df will be a cropped graph
+        # visualize the data and choose crop points
+        # the returned df will be a cropped graph
         data = sys.argv[1]
+        # data --> o1s.csv 
         df = cropMyGraph(data)
